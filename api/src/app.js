38178337244f3bfd25aa6e70ee193b58dea412f4
Laoset <xlaospt2@gmail.com>
2023-01-23 21:18:24 -0300

    newVercelReq

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use((req, res, next) => {
   //MODIFICO PARA RUTEAR A MI FRONT EN VERCEL
   res.header(
     "Access-Control-Allow-Origin",
-    "https://deploy-perritos.vercel.app/"
+    "https://deploy-perritos.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
