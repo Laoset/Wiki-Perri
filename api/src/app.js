cb25f1d43d5df06a12d4e050c7b9bfdff98a8be8
Laoset <xlaospt2@gmail.com>
2023-01-26 15:37:53 -0300

    finalPush

--- api/src/app.js
+++ api/src/app.js
@@ -18,6 +18,7 @@ server.use((req, res, next) => {
   //MODIFICO para que pueda hacer peticiones a la URL escrita
   res.header(
     "Access-Control-Allow-Origin",
+    // "http://localhost:3000"
     "https://deploy-perritos.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
