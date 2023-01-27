a0b30a488bd9ca139484d6e934ab8271c543b102
Laoset <xlaospt2@gmail.com>
2023-01-26 22:14:49 -0300

    pep

--- client/src/reduxActions/actions.js
+++ client/src/reduxActions/actions.js
@@ -3,8 +3,8 @@ import Axios from "axios";
 export const getRazas = () => {
   return async function (dispatch) {
     let infoAfuera = await Axios.get(
-      // "https://deployperritos-production.up.railway.app/dogs"
-      "http://localhost:3001/dogs"
+      "https://deployperritos-production.up.railway.app/dogs"
+      // "http://localhost:3001/dogs"
     );
     return dispatch({
       type: "GET_DOGS",
@@ -16,8 +16,8 @@ export const getRazas = () => {
 export const getTemperaments = () => {
   return async function (dispatch) {
     let infoTemperaments = await Axios.get(
-      // "https://deployperritos-production.up.railway.app/temperaments"
-      "http://localhost:3001/temperaments"
+      "https://deployperritos-production.up.railway.app/temperaments"
+      // "http://localhost:3001/temperaments"
     );
     return dispatch({
       type: "GET_TEMPERAMENTS",
@@ -29,8 +29,8 @@ export const getTemperaments = () => {
 export const searchDog = (payload) => {
   return async function (dispatch) {
     let dataPerri = await Axios.get(
-      // `https://deployperritos-production.up.railway.app/dogs?name=${payload}`
-      `http://localhost:3001/dogs?name=${payload}`
+      `https://deployperritos-production.up.railway.app/dogs?name=${payload}`
+      // `http://localhost:3001/dogs?name=${payload}`
     );
     return dispatch({
       type: "SEARCH_DOG",
@@ -42,8 +42,8 @@ export const searchDog = (payload) => {
 export const searchIdDog = (id) => {
   return async function (dispatch) {
     let dataPerriId = await Axios.get(
-      // `https://deployperritos-production.up.railway.app/dogs/${id}`
-      `http://localhost:3001/dogs/${id}}`
+      `https://deployperritos-production.up.railway.app/dogs/${id}`
+      // `http://localhost:3001/dogs/${id}}`
     );
     return dispatch({
       type: "SEARCH_ID",
@@ -81,8 +81,8 @@ export const orderByWeight = (payload) => {
 export const postDog = (payload) => {
   return async function (dispatch) {
     const postPerri = await Axios.post(
-      // "https://deployperritos-production.up.railway.app/dogs",
-      "http://localhost:3001/dogs",
+      "https://deployperritos-production.up.railway.app/dogs",
+      // "http://localhost:3001/dogs",
 
       payload
     );
