8574c6c6a4f672e2e620983dca1023934e420efb
Laoset <xlaospt2@gmail.com>
2023-01-29 17:52:25 -0300

    fixedDevTools

--- client/src/reduxStore/index.js
+++ client/src/reduxStore/index.js
@@ -1,11 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import thunk from "redux-thunk";
 import rootReducer from "../reduxReducer/reducer";
 
 export const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunkMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(thunk))
 );
