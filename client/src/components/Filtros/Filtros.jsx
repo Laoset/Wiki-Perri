b5347ed19a8a66ee940f88b878140d7e7d89159c
Laoset <xlaospt2@gmail.com>
2023-01-29 14:09:23 -0300

    nuevo

--- client/src/components/Filtros/Filtros.jsx
+++ client/src/components/Filtros/Filtros.jsx
@@ -33,9 +33,7 @@ export const Filtros = ({ setCurrentPage, setOrden }) => {
   // });
   // console.log(uniqueTemps);
   //Filtrado limpiando los vacios
-  let uniqueTempsSinVacio = uniqueNames.filter(
-    (temp) => temp.name.trim() !== ""
-  );
+  let uniqueTempsSinVacio = uniqueNames.filter((temp) => temp.name !== "");
   console.log(uniqueTempsSinVacio);
   //Cuando mi componente se monta: ejecuta lo siguiente
   useEffect(() => {
