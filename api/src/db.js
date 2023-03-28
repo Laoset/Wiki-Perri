6850c51a1ff857464bc00d23c4b2f37ef481930b
Laoset <xlaospt2@gmail.com>
2023-03-28 18:05:10 -0300

    newBase

--- api/src/db.js
+++ api/src/db.js
@@ -2,14 +2,17 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const { DB } = process.env;
+const sequelize = new Sequelize(`${DB}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
