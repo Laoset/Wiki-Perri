3fd4a72955d4fcaef83b608ce200261a7fa21e55
Laoset <xlaospt2@gmail.com>
2023-05-02 22:32:41 -0300

    re

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { PORT } = process.env;
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
   });
